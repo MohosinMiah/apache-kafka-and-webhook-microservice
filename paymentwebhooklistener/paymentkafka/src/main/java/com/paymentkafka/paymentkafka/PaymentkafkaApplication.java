c4c8d0563ffa260af0a4965a5b509a9c69e1bf71
mohosinmiah <hamza1610330816@gmail.com>
2023-06-03 21:38:36 +0600

    Initail Project Setup

---
+++ paymentwebhooklistener/paymentkafka/src/main/java/com/paymentkafka/paymentkafka/PaymentkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.paymentkafka.paymentkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentkafkaApplication.class, args);
+	}
+
+}
---
+++ paymentwebhooklistener/paymentkafka/src/test/java/com/paymentkafka/paymentkafka/PaymentkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentkafka.paymentkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
